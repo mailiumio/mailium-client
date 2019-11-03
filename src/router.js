2e7127ea9b3d9877ecd5192239659e3e77a1c2ec
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2019-11-03 12:38:05 +0100

    Add auth middleware

--- src/router.js
+++ src/router.js
@@ -7,9 +7,11 @@ import Dashboard from '@/pages/Dashboard'
 import AuthSuccess from '@/pages/auth/AuthSuccess'
 import Login from '@/pages/auth/Login'
 
+import auth from '@/middleware/auth'
+
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
     mode: 'history',
     base: process.env.BASE_URL,
     routes: [
@@ -26,7 +28,10 @@ export default new Router({
         {
             path: '/dashboard',
             name: 'dashboard',
-            component: Dashboard
+            component: Dashboard,
+            meta: {
+                middleware: auth
+            }
         },
         {
             path: '/',
@@ -35,3 +40,36 @@ export default new Router({
         }
     ]
 })
+
+const nextFactory = (context, middleware, index) => {
+    const subsequentMiddleware = middleware[index]
+    if (!subsequentMiddleware) return context.next
+
+    return (...parameters) => {
+        context.next(...parameters)
+        const nextMiddleware = nextFactory(context, middleware, index)
+        subsequentMiddleware({ ...context, next: nextMiddleware })
+    }
+}
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.middleware) {
+        const middleware = Array.isArray(to.meta.middleware)
+            ? to.meta.middleware
+            : [to.meta.middleware]
+
+        const context = {
+            from,
+            next,
+            router,
+            to
+        }
+        const nextMiddleware = nextFactory(context, middleware, 1)
+
+        return middleware[0]({ ...context, next: nextMiddleware })
+    }
+
+    return next()
+})
+
+export default router
