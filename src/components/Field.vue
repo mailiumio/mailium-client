32a6828564ef26ae1b08a8f4d9fe5f5b72e3d647
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2019-11-17 14:10:55 +0100

    Create field component

---
+++ src/components/Field.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <label v-if="label" class="block text-xs text-gray-600 font-medium mb-1">{{ label }}</label>
+        <input
+            :type="type"
+            :placeholder="placeholder"
+            class="block w-full px-5 py-3 rounded border text-sm focus:outline-none focus:border-gray-400"
+            :class="{ 'border-gray-200': errors.lenght === 0, 'border-red-500': errors.length > 0 }"
+        />
+        <ul v-if="errors.length">
+            <li v-for="(error, i) in errors" :key="i" class="text-sm text-red-500 mt-1">{{ error }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            type: {
+                type: String,
+                default: 'text',
+            },
+            placeholder: String,
+            label: String,
+            errors: {
+                type: Array,
+                default: () => [],
+            },
+        },
+    }
+</script>
