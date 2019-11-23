f3dd3c7cfb83b66c04a3afdb846de193836f43d2
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2019-11-23 14:58:51 +0100

    Basic Grid component

---
+++ src/components/Grid.vue
@@ -0,0 +1,29 @@
+<template>
+    <div
+        class="grid"
+        :style="{ 'grid-template-columns': `repeat(${columns}, 1fr)`, 'grid-gap': `${gap}rem` }"
+    >
+        <slot />
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            columns: {
+                type: Number,
+                default: 6,
+            },
+            gap: {
+                type: Number,
+                default: 2,
+            },
+        },
+    }
+</script>
+
+<style scoped>
+    .grid {
+        display: grid;
+    }
+</style>
---
+++ src/components/GridColumn.vue
@@ -0,0 +1,16 @@
+<template>
+    <div :style="{ 'grid-column': `span ${span}` }">
+        <slot />
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            span: {
+                type: Number,
+                default: 1,
+            },
+        },
+    }
+</script>
