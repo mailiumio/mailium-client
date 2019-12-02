b5ba016b77a80ec631056fe2ed992c26207b2d93
Bjorn Lindholm <bjornlindholmhansen@gmail.com>
2019-12-02 10:09:24 +0100

    Rename types

--- src/store/teams.js
+++ src/store/teams.js
@@ -1,8 +1,8 @@
 import API from '@/api'
 
 const types = {
-    GET_TEAMS_SUCCESS: 'GET_TEAMS_SUCCESS',
-    SET_CURRENT_TEAM: 'SET_CURRENT_TEAM'
+    GET_TEAMS: 'GET_TEAMS',
+    SET_CURRENT_TEAM: 'SET_CURRENT_TEAM',
 }
 
 export default {
@@ -11,22 +11,22 @@ export default {
         entities: {},
         current: {
             id: null,
-            action: 'loading'
-        }
+            action: 'loading',
+        },
     },
     getters: {
         team: state =>
-            state.current.id ? state.entities[state.current.id] : null
+            state.current.id ? state.entities[state.current.id] : null,
     },
     mutations: {
-        [types.GET_TEAMS_SUCCESS](state, teams) {
+        [types.GET_TEAMS](state, teams) {
             state.entities = Object.fromEntries(
                 teams.map(team => [team.id, team])
             )
         },
         [types.SET_CURRENT_TEAM](state, team) {
             state.current.id = team.id
-        }
+        },
     },
     actions: {
         async getTeams({ commit, state }) {
@@ -35,7 +35,7 @@ export default {
                     '/teams'
                 )
 
-                commit(types.GET_TEAMS_SUCCESS, teams)
+                commit(types.GET_TEAMS, teams)
 
                 if (!state.current.id) {
                     commit(types.SET_CURRENT_TEAM, teams[0])
@@ -43,6 +43,6 @@ export default {
             } catch (error) {
                 console.log(error)
             }
-        }
-    }
+        },
+    },
 }
